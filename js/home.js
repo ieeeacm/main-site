a7e0fb3367ba848badb3ff54ea0d6b9217882a92
Tenzin Ukyab <tsu@mit.edu>
2017-03-17 17:14:01 -0400

    ?

--- js/home.js
+++ js/home.js
@@ -16,7 +16,7 @@ $(document).ready(function() {
         {id: 'KaiAichholz', name: 'Kai Aichholz', position: 'Faculty Chair', image: 'KaiAichholz_2017_6-2_FacultyCo-Chair.jpg'},
         {id: 'ShramanRayChaudhuri', name: 'Shraman Ray Chaudhuri', position: 'Faculty Chair', image: 'ShramanRayChaudhuri_2017_6-3_FacultyCo-Chair.jpg'},
         {id: 'CarlaPinzon', name: 'Carla Pinzon', position: 'Publicity Chair', image: 'CarlaPinzon_2018_6-1_PubChair.jpg'},
-        {id: 'JessicaWang', name: 'Jessica Wang', position: 'Publicity Chair', image: 'JessicaWang_2017_6-3_Publicity Chair.jpg'}
+        {id: 'JessicaWang', name: 'Jessica Wang', position: 'Publicity Chair', image: 'JessicaWang_2017_6-3_Publicity Chair.jpg'},
         {id: 'Anelise Newman', name: 'AneliseNewman', position: 'URGE Chair', image: 'AneliseNewman_2019_6-3_URGEchair.jpg'},
         {id: 'AndrewLuo', name: 'Andrew Luo', position: 'ML Chair', image: 'AndrewLuo_2019_6-3_MLGroup.jpg'},
         {id: 'WeiLow', name: 'Wei Low', position: 'Voltage Chair', image: 'WeiLow_2017_6-2_VoltageChair.jpg'},
