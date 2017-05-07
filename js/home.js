62bbe46d2afc65eec121edc97f33a142c8810b52
Tenzin Ukyab <tsu@mit.edu>
2017-05-07 13:35:38 -0400

    fix image size

--- js/home.js
+++ js/home.js
@@ -14,16 +14,17 @@ $(document).ready(function() {
       {id: 'MartinWinton', name: 'Martin Winton', position: 'Treasurer', image: 'MartinWinton_2020_6-2_Treasurer.jpg'},
       {id: 'SylvieLee', name: 'Sylvie Lee', position: 'Social Chair', image: 'SylvieLee_2019_6-2_Social.jpg'},
       {id: 'KaiAichholz', name: 'Kai Aichholz', position: 'Faculty Chair', image: 'KaiAichholz_2017_6-2_FacultyCo-Chair.jpg'},
-      {id: 'ShramanRayChaudhuri', name: 'Shraman Ray Chaudhuri', position: 'Faculty Chair', image: 'ShramanRayChaudhuri_2017_6-3_FacultyCo-Chair.jpg'},
       {id: 'CarlaPinzon', name: 'Carla Pinzon', position: 'Publicity Chair', image: 'CarlaPinzon_2018_6-1_PubChair.jpg'},
       {id: 'JessicaWang', name: 'Jessica Wang', position: 'Publicity Chair', image: 'JessicaWang_2017_6-3_Publicity Chair.jpg'},
-      {id: 'AneliseNewman', name: 'Anelise Newman', position: 'URGE Chair', image: 'AneliseNewman_2019_6-3_URGEchair.jpg'},
       {id: 'AndrewLuo', name: 'Andrew Luo', position: 'ML Chair', image: 'AndrewLuo_2019_6-3_MLGroup.jpg'},
       {id: 'WeiLow', name: 'Wei Low', position: 'Voltage Chair', image: 'WeiLow_2017_6-2_VoltageChair.jpg'},
-      {id: 'ChunchunWu', name: 'Chunchun Wu', position: '6# Chair', image: 'ChunchunWu_2018_6-2_6Sharpco-chair.jpg'},
       {id: 'JoseSoto', name: 'Jose Soto', position: '6# Chair', image: 'JoseSoto_2020_6-2_15_6SHarpCo-Chair.jpg'},
       {id: 'MinaSun', name: 'Mina Sun', position: 'URTC Chair', image: 'MinaSun_2019_6-2_URTC_Co-chair.jpg'},
-      {id: 'JacquelineXu', name: 'Jacqueline Xu', position: 'WiEECS Chair', image: 'JacquelineXu_2017_6-3_WomenInEECSChair.jpg'}
+      {id: 'JacquelineXu', name: 'Jacqueline Xu', position: 'WiEECS Chair', image: 'JacquelineXu_2017_6-3_WomenInEECSChair.jpg'},
+      {id: 'AneliseNewman', name: 'Anelise Newman', position: 'URGE Chair', image: 'AneliseNewman_2019_6-3_URGEchair.jpg'},
+      {id: 'ShramanRayChaudhuri', name: 'Shraman Ray Chaudhuri', position: 'Faculty Chair', image: 'ShramanRayChaudhuri_2017_6-3_FacultyCo-Chair.jpg'},
+      {id: 'ChunchunWu', name: 'Chunchun Wu', position: '6# Chair', image: 'ChunchunWu_2018_6-2_6Sharpco-chair.jpg'},
+      {id: 'TenzinUkyab', name: 'Tenzin Ukyab', position: 'Webmaster', image: 'TenzinUkyab_2020_6-3_Webmaster.jpg'}
     ];
 
 
@@ -32,7 +33,7 @@ $(document).ready(function() {
     var execCards = execHandlebars({officers: officers});
     $('#exec.row').html(execCards);
 
-    
+
     // SET UP EVENTS SECTION
     var events = $.grabCalendar({
         type: 'detailedEvents',
